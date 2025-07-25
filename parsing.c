00f5515cf409561f903db83e77d5f38b2fbef1bc
Nour-eddine Sahimi <nosahimi@c1r2p8.1337.ma>
2025-07-25 21:28:08 +0100

    scripted commit

--- parsing.c
+++ parsing.c
@@ -6,14 +6,11 @@
 /*   By: nosahimi <nosahimi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 10:53:41 by nosahimi          #+#    #+#             */
-/*   Updated: 2025/07/25 10:17:45 by nosahimi         ###   ########.fr       */
+/*   Updated: 2025/07/25 18:42:36 by nosahimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-
-
 
 t_cmd *parsing(char *line, t_env *env)
 {
